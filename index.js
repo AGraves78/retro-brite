488c3b0ebac1103c193ebe3d0af3672f9e95daab
Angela Graves <algraves78@gmail.com>
2017-06-14 20:59:44 -0400

    added the cell constructor that will allow the population of the grid more easily

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 const port = 8080
 
-server.use(express.static(__dirname + '/puclic'));
+server.use(express.static(__dirname + '/public'));
 
 server.get('/', (request, response) => {
   response.sendFile('public/html/index.html', { root: __dirname });
